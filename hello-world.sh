6e8879d316944b14bdd5d0017879b7523801df03
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-01-29 15:26:51 +0530

    file name modified

---
+++ hello-world.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# this is commented line, will not be executed by the shell
+echo "Hello World from the Rajeshirrinki" # echo used to print text on the console
+echo "Login as $USER" # this will print the username of the logged in user 
+date # this will print the current date and time
+echo 2+5 # this will print the result of arthimetic operation
+#End of the script
+
