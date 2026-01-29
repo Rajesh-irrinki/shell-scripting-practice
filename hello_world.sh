5969695d8150a1e0ff6357a6f66e68fea06341a4
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-01-29 15:19:27 +0530

    corrected the expression

--- hello_world.sh
+++ hello_world.sh
@@ -4,6 +4,6 @@
 echo "Hello World from the Rajeshirrinki" # echo used to print text on the console
 echo "Login as $USER" # this will print the username of the logged in user 
 echo $DATE 
-echo $(2+5) # this will print the result of arthimetic operation
+echo 2+5 # this will print the result of arthimetic operation
 #End of the script
 
