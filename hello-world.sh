70a7317bfe0b1369c4282c2037390ba943a54c37
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-08 15:30:17 +0530

    scripted is updated

--- hello-world.sh
+++ hello-world.sh
@@ -4,6 +4,6 @@
 echo "Hello World from the Rajeshirrinki" # echo used to print text on the console
 echo "Login as $USER" # this will print the username of the logged in user 
 date # this will print the current date and time
-echo 2+5 # this will print the result of arthimetic operation
+SUM=$((2+5)) # this will perfrom arthimetic operation
+echo $SUM
 #End of the script
-
