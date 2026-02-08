530f5686be0ef5de98ec32830c19f8a12d4a5df1
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-08 16:09:28 +0530

    variables.sh script created

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Rajesh
+PERSON2=Param
+
+echo "$PERSON1: Hi $PERSON2, How are you ?"
+echo "$PERSON2: Hi $PERSON1!, I am fine, how are you doing man ??"
+echo "$PERSON1: yeah, I'm doing great."
+echo "$PERSON2: Now tell me, what are you learning these days for Upskilling?"
+echo "$PERSON1: I am currently brushing up on my DevOps skills"
