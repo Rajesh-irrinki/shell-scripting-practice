f6ec0ef18e046333a57207c19cef1bc2703b3d9e
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-08 16:22:00 +0530

    args_variables.sh script created

--- args_variables.sh
+++ args_variables.sh
@@ -8,3 +8,7 @@ echo "$PERSON2: Hi $PERSON1!, I am fine, how are you doing man ??"
 echo "$PERSON1: yeah, I'm doing great."
 echo "$PERSON2: Now tell me, what are you learning these days for Upskilling?"
 echo "$PERSON1: I am currently brushing up on my DevOps skills"
+
+echo "File Name: $0" #prints the file name
+echo "Arguments are : $@"
+echo "toatl arguments: $# "  
