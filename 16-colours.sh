87e1d1511e83cfd4d87ee1319bc8b084a4efbe2b
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-09 01:28:16 +0530

    16-colours.sh script created

--- 16-colours.sh
+++ 16-colours.sh
@@ -4,5 +4,34 @@ N="\e[0m"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+log_folder=/var/log/shell
+log_file=$log_folder/$0.log
+user_id=$UID
 
-echo -e "$Y My$N $G name is $N: $R Rajesh$N"
+if [ $user_id -ne 0 ]; then
+    echo "$R Please run the script as root user$N"
+    exit 1;
+fi
+
+mkdir -p $log_folder
+
+validate_installation() {
+    if [ $1 -ne 0 ]; then
+        echo "Installing $2 is .......... $R FAILURE$N" | tee -a $log_file
+        exit 1;
+    else
+        echo "Installing $2 is ..........$G SUCCESS$N" | tee -a $log_file
+    fi
+}
+
+for package in $@
+do
+   dnf list installed $package &>> $log_file
+   if [ $? -eq 0 ]; then
+        echo "$package already installed ....$Y SKIPPING$N" | tee -a $log_file 
+    else 
+        echo "############## Installing $package ##############" &>> $log_file
+        dnf install $package -y &>> $log_file
+        validate_installation $? $package
+    fi
+done
