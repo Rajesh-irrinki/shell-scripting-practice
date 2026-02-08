ef61527635917d6ccb71b76ddd2073165bc36176
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-08 16:00:57 +0530

    conversation.sh scripted created

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Rajesh: Hi Param, How are you ?"
+echo "Param: Hi Rajesh!, I am fine, how are you doing man ??"
+echo "Rajesh: yeah, I'm doing great."
+echo "Param: Now tell me, what are you learning these days for Upskilling?"
+echo "Rajesh: I am currently brushing up on my DevOps skills"
