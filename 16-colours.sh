76bcfab7817cf27d7a59daab5f561d24c0c0321d
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-09 01:29:22 +0530

    16-colours.sh script created

--- 16-colours.sh
+++ 16-colours.sh
@@ -9,7 +9,7 @@ log_file=$log_folder/$0.log
 user_id=$UID
 
 if [ $user_id -ne 0 ]; then
-    echo "$R Please run the script as root user$N"
+    echo -e "$R Please run the script as root user$N"
     exit 1;
 fi
 
@@ -17,10 +17,10 @@ mkdir -p $log_folder
 
 validate_installation() {
     if [ $1 -ne 0 ]; then
-        echo "Installing $2 is .......... $R FAILURE$N" | tee -a $log_file
+        echo -e "Installing $2 is .......... $R FAILURE$N" | tee -a $log_file
         exit 1;
     else
-        echo "Installing $2 is ..........$G SUCCESS$N" | tee -a $log_file
+        echo -e "Installing $2 is ..........$G SUCCESS$N" | tee -a $log_file
     fi
 }
 
@@ -28,7 +28,7 @@ for package in $@
 do
    dnf list installed $package &>> $log_file
    if [ $? -eq 0 ]; then
-        echo "$package already installed ....$Y SKIPPING$N" | tee -a $log_file 
+        echo -e "$package already installed ....$Y SKIPPING$N" | tee -a $log_file 
     else 
         echo "############## Installing $package ##############" &>> $log_file
         dnf install $package -y &>> $log_file
