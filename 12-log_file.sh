664fb77b23e0bfa48b62d86b524bdb5f2695189a
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-08 21:44:58 +0530

    12-log_file.sh scripted added

--- 12-log_file.sh
+++ 12-log_file.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir -p /var/log/shell
 log_folder=/var/log/shell
 log_file=$log_folder/$0.log
 user_id=$UID
@@ -10,23 +9,25 @@ if [ $user_id -ne 0 ]; then
     exit 1;
 fi
 
+mkdir -p $log_folder
+
 validate_installation () {
     if [ $1 -eq 0 ]; then
-        echo "Installing $2 .......... SUCCESS" &>> $log_file
+        echo "Installing $2 .......... SUCCESS" | tee -a $log_file
     else
-        echo "Installing $2 .......... FAILURE" &>> $log_file
+        echo "Installing $2 .......... FAILURE" | tee -a $log_file
         exit 1;
     fi
 }
 
 echo "#########################  Installing Nginx #########################"
-dnf install nginx -y  >> $log_file 
+dnf install nginx -y  &>> $log_file 
 validate_installation $? Nginx 
 
 echo "#########################  Installing MySQL #########################"
-dnf install mysql -y >> $log_file
+dnf install mysql -y &>> $log_file
 validate_installation $? MySQL 
 
 echo "######################### Installing NodeJs #########################"
-dnf install nodejs -y >> $log_file
+dnf install nodejs -y &>> $log_file
 validate_installation $? NodeJs 
