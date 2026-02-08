c15d9675b8c2687ddfd42537eae63d2948aba321
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-08 21:19:30 +0530

    11-function.sh.sh scripted added

---
+++ 11-function.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+user_id=$UID
+if [ $user_id -ne 0 ]; then
+    echo "Please run the script as root user"
+    exit 1;
+fi
+
+validate_installation () {
+    if [ $1 -eq 0 ]; then
+        echo "Installing $2 .......... SUCCESS"
+    else
+        echo "Installing $2 .......... FAILURE"
+}
+
+echo "Installing Nginx ......."
+dnf install nginx -y
+validate_installation $? Nginx
+
+echo "Installing MySQL ........"
+dnf install mysql -y
+validate_installation $? MySQL
+
+echo "Installing NodeJs ........"
+dnf install nodejs -y
+validate_installation $? NodeJs
