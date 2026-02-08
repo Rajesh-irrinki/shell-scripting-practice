94b356e3bbccbdc3c9d6d3967b6685f39247e7e8
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-09 02:53:25 +0530

    18-trap.sh script created

---
+++ 18-trap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e #ERR
+trap 'echo "There is an error at $LINENO, command: $BASH_COMMAND" ' 
+
+echo "Hello World!!"
+echo "I am learning shell scripting"
+echoo "this line have error"
+echo "No error in this"
