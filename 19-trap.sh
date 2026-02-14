7bab81b67a044fc4f0c8f7b2dbf12d50e8d33112
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-15 00:18:19 +0530

    19-trap.sh script created

---
+++ 19-trap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e #ERR
+trap 'echo "there is an error while executing the script at $LINENO line number and commnd : $BASH_COMMAND"'  ERR
+
+user_id=$UID
+log_folder=/var/log/shell/
+log_file=$log_folder/$0.log
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+
+if [ user_id -ne 0] ; then
+    echo -e " $R Please run the script with root access $N " | tee -a $log_file
+    exit 1;
+fi
+
+mkdir -p $log_folder
+
+for package in $@
+do
+    dnf list installed $package
+    if [ $? -ne 0 ]; then
+        echo -e "$package is not installed, ........ $G INSTALLING NOW $N" &>> $log_file
+        dnf install $package &>> $log_file &>> $log_file
+    else
+        echo -e "$package is already installed ......$Y SKIPPING $N " &>> $log_file
+    fi
+done
