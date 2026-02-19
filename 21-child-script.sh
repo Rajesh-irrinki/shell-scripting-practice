50f99e92bab2d9639f885610d1c21dca91bbe66d
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-20 02:31:40 +0530

    scripts added to shell-scripting-pratice repo

--- 21-child-script.sh
+++ 21-child-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_dir=$(cd "$(dirname "$0")" && pwd)
 echo " " [ $(date '+%F %T') ] " child script is executing started...."
-for i in 5 
+for i in {1..5} 
 do 
     echo "" [ $(date '+%F %T') ] " Count: $i"
     sleep 10;
