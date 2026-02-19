d60e1bd7034106afee0f660e304cddaed20f0256
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-20 02:49:21 +0530

    scripts added to shell-scripting-pratice repo

--- 20-parent-script.sh
+++ 20-parent-script.sh
@@ -6,6 +6,6 @@ echo " " [ $(date '+%F %T') ] " $PWD"
 echo " " [ $(date '+%F %T') ] " Parent script is calling child script.."
 source ./21-child-script.sh
 cd $script_dir # variable will be sourced from child script
-echo " " [ $(date '+%F %T') ] " $pwd"
+echo " " [ $(date '+%F %T') ] " $PWD"
 end_time=$(date +%s)
 echo " Script exectuion time : $(($end_time - $start_time)) "
