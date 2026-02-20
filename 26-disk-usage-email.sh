10d46f18edbb2c44dfe8eee4ec5328944adbc04c
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-21 02:08:14 +0530

    scripts added to shell-scripting-pratice repo

--- 26-disk-usage-email.sh
+++ 26-disk-usage-email.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 Message=""
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 usage_threshold="20"
 
 disk_usage=$(df -hT | grep -v Filesystem)
@@ -10,8 +11,10 @@ do
     usage=$(echo $line | awk '{print $6}' | cut -d '%' -f1 )
     partition=$(echo $line | awk '{print $7}')
     if [ $usage -ge $usage_threshold ]; then
-        Message+="High disk usage on $partition : $usage \n"
+        Message+="High disk usage on $partition : $usage <br>"
     fi
 done <<< $disk_usage
 
 echo -e "$Message"
+
+sh 26-mail.sh "rajeshirrinki321@gmai.com" "High Disk Usage Alert on $IP_ADDRESS" "$Message" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
