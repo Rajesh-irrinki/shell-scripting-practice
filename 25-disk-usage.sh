e3de602dc878cb3c21edc90c13f6ef3aa3022404
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-21 00:07:23 +0530

    scripts added to shell-scripting-pratice repo

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -7,7 +7,7 @@ disk_usage=$(df -hT | grep -v Filesystem)
 
 while IFS= read -r line
 do
-    if [ $usage -ge $threshold ]; then
+    if [ "$usage" -ge "$threshold" ]; then
         usage=$($line | awk '{print $6}' | cut -d "%" -f1)
         partition=$($line | awk '{print $7}')
         Message+="High disk usage on $partition : $usage"
